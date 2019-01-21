8c800028fd28f18ce331924884ecfc83924b5ff7
joachimprinzbach <joachim.prinzbach@gmail.com>
2019-01-21 14:17:22 +0100

    Add custom healthCheck

--- src/main/java/ch/springboot/fundamentals/pizzaservice/domain/CatalogService.java
+++ src/main/java/ch/springboot/fundamentals/pizzaservice/domain/CatalogService.java
@@ -3,13 +3,18 @@ package ch.springboot.fundamentals.pizzaservice.domain;
 import ch.springboot.fundamentals.pizzaservice.infrastructure.CatalogEntry;
 import ch.springboot.fundamentals.pizzaservice.infrastructure.CatalogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
 
 @Service
-public class CatalogService {
+public class CatalogService implements HealthIndicator, InfoContributor {
 
     private final CatalogRepository catalogRepository;
 
@@ -26,6 +31,20 @@ public class CatalogService {
 
     public List<CatalogEntry> getAll() {
         return catalogRepository.findAll();
+    }
+
+    @Override
+	public Health health() {
+        if(catalogRepository.findAll().isEmpty()) {
+            return Health.outOfService().withDetail("errorDetail", "Error occured").build();
+        }
+        return Health.up().build();
+    }
+
+    @Override
+    public void contribute(Info.Builder builder) {
+        builder.withDetail("someInfo",
+                Collections.singletonMap("someKey", "someValue"));
     }
 
     private Predicate<CatalogEntry> isInStock() {
